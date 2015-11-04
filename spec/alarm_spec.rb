3157eb4d4f75dda121152ffcf4909cecb00fbcea
trikitrok <trikitrok@gmail.com>
2015-11-04 23:30:36 +0100

    Technique 1 to make Alarm testable: using monkey patching to inject a fake sensor

--- spec/alarm_spec.rb
+++ spec/alarm_spec.rb
@@ -1,11 +1,22 @@
 require_relative '../lib/alarm'
 
 describe Alarm do
-  it "is on" do
+  it "is on when pressure is too low" do
     alarm = Alarm.new
+    alarm.sensor = FakeSensor.new
 
     alarm.check
 
     expect(alarm.alarm_on).to be_truthy
+  end
+end
+
+class Alarm
+  attr_writer :sensor
+end
+
+class FakeSensor
+  def pop_next_pressure_psi_value
+    10
   end
 end
