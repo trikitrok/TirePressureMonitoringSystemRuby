0d4d8fd2fedba1d4301e5eaf14117551350b8ad5
trikitrok <trikitrok@gmail.com>
2015-11-07 02:04:22 +0100

    Made explicit that Sensor was really a PressureSensor

--- lib/pressure_sensor.rb
+++ lib/pressure_sensor.rb
@@ -1,4 +1,4 @@
-class Sensor
+class PressureSensor
   # The reading of the pressure value from the sensor is simulated in this implementation.
   # Because the focus of the exercise is on the other class.
 
