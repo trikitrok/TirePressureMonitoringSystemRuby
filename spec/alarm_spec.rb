bdcc699c1cf1b1f45296fba0676eeba4a621ce5d
trikitrok <trikitrok@gmail.com>
2015-11-07 01:06:50 +0100

    Test to check that the Alarm is off when pressure is inside the safety range

--- spec/alarm_spec.rb
+++ spec/alarm_spec.rb
@@ -2,16 +2,28 @@ require_relative '../lib/alarm'
 
 describe Alarm do
   it "is on when pressure is too low" do
-    alarm = FakeAlarm.new
+    alarm = FakeAlarm.new(8)
 
     alarm.check
 
     expect(alarm.alarm_on).to be_truthy
   end
 
+  it "is off when pressure is inside safety range" do
+    alarm = FakeAlarm.new(19)
+
+    alarm.check
+
+    expect(alarm.alarm_on).to be_falsy
+  end
+
   class FakeAlarm < Alarm
+    def initialize value
+      @value = value
+    end
+
     def sample_value
-      8
+      @value
     end
   end
 end
